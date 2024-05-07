428ae302fe7e7bdd2a4ca3a53ecae0d826421383
NNT.Koala <105761601+NNTKOALA@users.noreply.github.com>
2024-05-07 10:26:10 +0700

    change (done): file location

--- Assets/#Game/Scripts/Weapon/Weapon.cs
+++ Assets/#Game/Scripts/Weapon/Weapon.cs
@@ -79,6 +79,6 @@ public class Weapon : MonoBehaviour
     private void CreateHitImpact(RaycastHit hit)
     {
         GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
-        Destroy(impact, 0.1f);
+        Destroy(impact, 0.5f);
     }
 }
